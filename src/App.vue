d3e7d0a76e7555d70a1094a9eb68914fa1f7ec82
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-02-03 11:42:15 +0200

    dinamic comp & slots

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>
+      <button v-on:click="dinamicCmp = 'appA'">A</button>
+      <button v-on:click="dinamicCmp = 'appB'">B</button>
+      <button v-on:click="dinamicCmp = 'appC'">C</button>
+    </div>
+    <component v-bind:is="dinamicCmp">
+      <h5>Some title</h5>
+      <p>
+        {{ title }}
+      </p>
+    </component>
+    <app-quote>
+      <h5 slot="title">Some title</h5>
+      <p slot="content">
+        {{ title }}
+      </p>
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote";
+import A from "./A";
+import B from "./B";
+import C from "./C";
+
+export default {
+  data() {
+    return {
+      dinamicCmp: "appA",
+      title:
+        "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. At velit dolorexpedita amet. Ad, veritatis obcaecati accusamus deleniti voluptates inminima. Laboriosam odit expedita illum in beatae veniam at similique."
+    };
+  },
+  computed: {
+    witch() {
+      return this.cmp;
+    }
+  },
+  components: {
+    appQuote: Quote,
+    appA: A,
+    appB: B,
+    appC: C
+  }
+};
+</script>
---
+++ src/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <slot name="title"></slot>
+    </div>
+    <div>
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  margin: 5px;
+  text-align: center;
+  box-shadow: 1px 1px 1px 1px grey;
+  padding-bottom: 5px;
+}
+</style>
